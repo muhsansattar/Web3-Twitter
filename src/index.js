7d5eea482f3b610999d25eaa444428aa0679dda7
muhsansattar <muhsansattar97@gmail.com>
2024-09-28 15:20:40 +0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { HashRouter } from "react-router-dom";
+import { MoralisProvider } from "react-moralis";
+import { NotificationProvider } from 'web3uikit';
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MoralisProvider appId="83Reu8JlXgzEg9V1drN0OADuopa08eCSPLXz4czs" serverUrl="https://kivt14y98mje.usemoralis.com:2053/server">
+      <NotificationProvider>
+      <HashRouter>
+        <App />
+      </HashRouter>
+      </NotificationProvider>
+    </MoralisProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
